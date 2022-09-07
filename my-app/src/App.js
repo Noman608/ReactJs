c216c11a2752df1f4d908ecb6a2884bde65c9190
Abdullah Al Noman <68609158+Noman608@users.noreply.github.com>
2022-09-07 15:00:42 +0530

    noon update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// let el = <h1>Hello NoShow</h1>;        JSX
+// let el = React.createElement('h1',null,"Hello NoSite Show!!");
+
+class App extends Component {
+  render() {
+    // return React.createElement("h1", null, "Hello From the App Component!!");
+    return(
+        <h1>Hello ...</h1>
+    )
+  }
 }
 
 export default App;
