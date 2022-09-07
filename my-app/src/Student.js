dac245aadc7ebbdc27c1d0c63c2489dac200ee7c
Abdullah Al Noman <68609158+Noman608@users.noreply.github.com>
2022-09-07 20:32:05 +0530

    props in class

--- my-app/src/Student.js
+++ my-app/src/Student.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Student = (props)=>{
-    return(
-        <>
-            <h1>Hello {props.name}</h1>
-            <h1>Your Roll :{props.roll}</h1>
-        </>
-    )
+class Student extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello {this.props.name}</h1>
+        <h1>Your Roll :{this.props.roll}</h1>
+      </>
+    );
+  }
 }
 export default Student;
