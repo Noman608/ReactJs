a54a212c177090ebc94fbd2e6eb0e9c3448106ef
Abdullah Al Noman <68609158+Noman608@users.noreply.github.com>
2022-09-08 12:17:02 +0530

    State with constractor

--- my-app/src/Student.js
+++ my-app/src/Student.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 class Student extends Component {
-  state = {
-    name: "Noman",
-    roll: this.props.roll,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Noman",
+      roll: this.props.roll,
+    };
+  }
   render() {
-    return <h1>Hello {this.state.name} Your Roll number : {this.state.roll}</h1>;
+    return (
+      <h1>
+        Hello {this.state.name} Your Roll number : {this.state.roll}
+      </h1>
+    );
   }
 }
 
