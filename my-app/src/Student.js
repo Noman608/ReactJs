ea337d83415ebcbfbffc7622b58c95cafbee758d
Abdullah Al Noman <68609158+Noman608@users.noreply.github.com>
2022-09-08 19:18:17 +0530

    Event Handling with Class

--- my-app/src/Student.js
+++ my-app/src/Student.js
@@ -1,19 +1,28 @@
-import React, { Component } from "react";
-class Student extends Component {
-  constructor(props) {
+import React, { Component } from 'react'
+class Student extends Component{
+
+  constructor(props){
     super(props);
-    this.state = {
-      name: "Noman",
-      roll: this.props.roll,
-    };
+    this.state ={
+      name:"Rahul",
+      roll:this.props.roll
+    }
+    this.handleClick = this.handleClick.bind(this);
   }
-  render() {
-    return (
-      <h1>
-        Hello {this.state.name} Your Roll number : {this.state.roll}
-      </h1>
-    );
+   handleClick(){
+    console.log("Button Clicked",this);
+  }
+  //  handleClick =()=>{
+  //   console.log("Button Clicked",this);
+  // }
+
+  render(){
+    return(
+      <>
+        <h1>Hello {this.state.name} Your Roll Number is {this.state.roll}</h1>
+        <button onClick={this.handleClick}>Click Me</button>
+      </>
+    )
   }
 }
-
 export default Student;
