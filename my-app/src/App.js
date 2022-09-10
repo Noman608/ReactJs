0382be93972c8fb0ff174dbdbc4176f0c0b40bc7
Abdullah Al Noman <68609158+Noman608@users.noreply.github.com>
2022-09-10 20:51:13 +0530

    hook useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function App() {
-  
-  const [name,setName] = useState('Noman');
-  const [roll,setRoll] = useState(608);
-  const handleClick= ()=>{
-    setName('Abdullah Al Noman');
-    setRoll(1880608);
-  }
-    return (
-    <div>
-      <h1>Name:{name}</h1>
-      <h1>Roll:{roll}</h1>
-      <button type='button' onClick={handleClick}>Change</button>
-    </div>
-  )
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(50);
+  const handeleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handeleDcrement = () => {
+    setCount1(count1 - 1);
+  };
+  useEffect(() => {
+    console.log("<-----Use Effect Called----->");
+  }, [count1]);
+  return (
+    <React.Fragment>
+      <h1>Count up:{count}</h1>
+      <button type="button" onClick={handeleIncrement}>
+        Increment
+      </button>
+      <h1>Count Down:{count1}</h1>
+      <button type="button" onClick={handeleDcrement}>
+        Increment
+      </button>
+    </React.Fragment>
+  );
 }
