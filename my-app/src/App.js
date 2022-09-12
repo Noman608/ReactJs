20bf4ce98ba511f67b4f46a100e9f6b3ee75af22
Abdullah Al Noman <68609158+Noman608@users.noreply.github.com>
2022-09-12 13:43:44 +0530

    CSS module

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import "./App.css"
+import styles from "./App.module.css";
 import User from './User'
 export default class App extends Component {
   render() {
     let style = false;
     return (
       <div>
-        <h1 className='txtb textS'>Hello App</h1>
+        <h1 className={`${styles.textS} ${styles.txtg}`}>Hello App</h1>
         <User color={style?"txtb":"txtg"}/>
       </div>
     )
--- my-app/src/User.js
+++ my-app/src/User.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import styles from "./User.module.css";
 export default class User extends Component {
   render() {
     
     return (
       <div>
-        <h2 className={this.props.color}>Hello Users</h2>
+        <h2 className={styles.txtg}>Hello Users</h2>
       </div>
     )
   }
