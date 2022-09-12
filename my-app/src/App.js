87ca43086865343236610e10fdd62242d9e8498f
Abdullah Al Noman <68609158+Noman608@users.noreply.github.com>
2022-09-12 14:41:50 +0530

    Bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import pic from "./disha.jpg"
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        {/* <img src={process.env.PUBLIC_URL+"/img/disha.jpg"} alt="Mypic" /> */}
-        <img src={pic} alt="Mypic"/>
+        <button type="button" className="btn btn-danger">Danger Normal Button</button>
+
       </div>
     )
   }
